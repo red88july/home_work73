70b14f11920621d4cbf04c54f31c57dee5b3570f
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-01-10 15:07:07 +0600

    Using library Vigenere create encrypted server

--- taskTwo/task2.js
+++ taskTwo/task2.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const app = express();
-const port = 8001;
+const port = 8080;
+const Vigenere = require('caesar-salad').Vigenere;
 
-app.get('/test', (req, res) => {
-    res.send('This is a second task');
+const enCoddingWord = 'goods';
+const deCoddingWord = 'goodnight';
+
+app.get('/encode/:passEncode', (req, res) => {
+    const encodingPassword = req.params.passEncode;
+    const encryptPass = Vigenere.Cipher(enCoddingWord).crypt(encodingPassword);
+    res.send(encryptPass);
+});
+
+app.get('/decode/:passDeCode', (req, res) => {
+    const decodingPassword = req.params.passDeCode;
+    const decryptionPassword = Vigenere.Decipher(deCoddingWord).crypt(decodingPassword);
+    res.send(decryptionPassword);
 });
 
 app.listen(port, () => {
-   console.log(`Server is online on ${port}`);
+    console.log(`Server is online on ${port}`);
 })
