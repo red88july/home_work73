871d21f254872882defa35c7e2ee01ccf72e29e4
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-01-11 14:13:23 +0600

    Modify code(delete constant for password) enter pass on Vigenere.Cipher and Vigenere.Decipher

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ const port = 8000;
 
 const Vigenere = require('caesar-salad').Vigenere;
 
-const enCoddingKey = 'goods';
-const deCoddingKey = 'goodnight';
-
-
 app.get('/', (req, res) => {
     res.send('<h1>This is a default route </h1>' + `<h4>On this route can't find any content</h4>`);
 });
@@ -16,15 +12,15 @@ app.get('/Hello', (req, res) => {
     res.send('<h1>Hello!</h1>');
 });
 
-app.get('/encode/:passEncode', (req, res) => {
-    const encodingPassword = req.params.passEncode;
-    const encryptPass = Vigenere.Cipher(enCoddingKey).crypt(encodingPassword);
+app.get('/encode/:encode', (req, res) => {
+    const encodingPassword = req.params.encode;
+    const encryptPass = Vigenere.Cipher('password').crypt(encodingPassword);
     res.send(encryptPass);
 });
 
-app.get('/decode/:passDeCode', (req, res) => {
-    const decodingPassword = req.params.passDeCode;
-    const decryptionPassword = Vigenere.Decipher(deCoddingKey).crypt(decodingPassword);
+app.get('/decode/:decode', (req, res) => {
+    const decodingPassword = req.params.decode;
+    const decryptionPassword = Vigenere.Decipher('password').crypt(decodingPassword);
     res.send(decryptionPassword);
 });
 
