444c6e324665dce4de732bde2deefcccb286963d
Sergeev Mikhail <sergeev-michael@mail.ru>
2024-01-16 14:16:14 +0600

    Rewrite code line 11 on script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ app.get('/', (req, res) => {
     res.send('<h1>This is a default route </h1>' + `<h4>On this route can't find any content</h4>`);
 });
 
-app.get('/Hello', (req, res) => {
-    res.send('<h1>Hello!</h1>');
+app.get('/text/:word', (req, res) => {
+    res.send(req.params.word);
 });
 
 app.get('/encode/:encode', (req, res) => {
